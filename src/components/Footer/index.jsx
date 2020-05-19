60fb83acd851a8981b40944fa189d2e4ad29f2cb
jigglypop <ydh2244@gmail.com>
2020-05-20 01:46:26 +0900

    0520

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FooterWrapper } from './styled';
+
+const Footer = () => (
+  <FooterWrapper>
+    <h1>YDH</h1>
+  </FooterWrapper>
+);
+
+export default Footer;
