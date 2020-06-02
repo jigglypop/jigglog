ee3e4e94ce2cd20e30566f2de7dcd5ed3bd6d469
jigglypop <ydh2244@gmail.com>
2020-06-03 01:17:10 +0900

    0603

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const { TITLE, AUTHOR, SITE_URL } = require('./src/constants');
+const { TITLE, AUTHOR, SITE_URL } = require("./src/constants");
 
 const siteMetadata = {
   title: TITLE,
@@ -9,78 +9,83 @@ const siteMetadata = {
 
 module.exports = {
   siteMetadata,
-  pathPrefix: '/',
+  pathPrefix: "/",
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/resources`,
-        name: 'resources',
+        name: "resources",
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 640,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
+              wrapperStyle: "margin-bottom: 1.0725rem",
             },
           },
           {
-            resolve: 'gatsby-remark-prismjs',
+            resolve: "gatsby-remark-prismjs",
             options: {
-              classPrefix: 'hljs-',
+              classPrefix: "hljs-",
             },
           },
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
         ],
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: '<<YOUR_OWN_KEY>>',
+        trackingId: "<<YOUR_OWN_KEY>>",
       },
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-offline",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Gatsby Advanced Blog',
-        short_name: 'Gatsby Blog',
-        start_url: '/',
-        background_color: '#fff',
-        theme_color: '#3B9CFF',
-        display: 'minimal-ui',
-
+        name: "Gatsby Advanced Blog",
+        short_name: "Gatsby Blog",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#3B9CFF",
+        display: "minimal-ui",
       },
     },
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
         host: SITE_URL,
         sitemap: `${SITE_URL}/sitemap.xml`,
-        policy: [{ userAgent: '*', allow: '/' }],
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-favicon",
+      options: {
+        logo: "./moon.png",
       },
     },
     {
