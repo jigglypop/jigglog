a8d506db30a54fa6d772c0d9a05aeb19247a8168
jigglypop <ydh2244@gmail.com>
2020-06-03 01:42:30 +0900

    0603

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,27 +1,25 @@
 exports.CONTENT_PER_PAGE = 8;
 exports.PAGE_PER_SCREEN = 4;
 
-exports.POST = 'post';
-exports.PORTFOLIO = 'portfolio';
-exports.RESUME = 'resume';
+exports.POST = "post";
+exports.PORTFOLIO = "portfolio";
+exports.RESUME = "resume";
 
 // Site Information
-exports.NAME = 'jigglog';
-exports.TITLE = 'jigglog';
-exports.PREFIX = 'jigglog | ';
-exports.DESCRIPTION = 'jigglypop\'s blog';
-exports.AUTHOR = 'jigglypop';
-exports.EMAIL = 'ydh2244@gmail.com';
-exports.KEYWORDS = [
-  'Gatsby',
-  'ReactJS',
-].join(', ');
-exports.SITE_URL = 'https://github.com/jigglypop';
-exports.PROFILE = 'https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/80752915_590524238390867_3447356711315177472_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=NF9jd1Uy0CwAX8Vj4wW&oh=070f05f919f6051897af2b9ce60957a4&oe=5EF58127';
+exports.NAME = "jigglog";
+exports.TITLE = "jigglog";
+exports.PREFIX = "jigglog | ";
+exports.DESCRIPTION = "jigglypop's blog";
+exports.AUTHOR = "jigglypop";
+exports.EMAIL = "ydh2244@gmail.com";
+exports.KEYWORDS = ["Gatsby", "ReactJS"].join(", ");
+exports.SITE_URL = "https://jigglypopownprivateblog.netlify.app";
+exports.PROFILE =
+  "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/80752915_590524238390867_3447356711315177472_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=NF9jd1Uy0CwAX8Vj4wW&oh=070f05f919f6051897af2b9ce60957a4&oe=5EF58127";
 
 // Social
-exports.GITHUB_ID = 'jigglypop';
-exports.FACEBOOK_ID = 'donghwan.yeom.3';
+exports.GITHUB_ID = "jigglypop";
+exports.FACEBOOK_ID = "donghwan.yeom.3";
 
 // Disqus (Recommend you to use this)
-exports.DISQUS_ID = 'django-english-mentormentee';
+exports.DISQUS_ID = "django-english-mentormentee";
