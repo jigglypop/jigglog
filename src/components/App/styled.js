c524cd15bfc1476d5b3d35c3fafee68cf1a451cb
jigglypop <ydh2244@gmail.com>
2020-07-08 08:36:25 +0900

    0708

--- src/components/App/styled.js
+++ src/components/App/styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  background: linear-gradient(45deg, #c31432, #240b36) !important;
+
   @font-face {
     font-family: "NanumBarunGothic";
     src: url("../../fonts/NanumBarunGothic.ttf");
