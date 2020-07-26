e83b603c233196a0c3fcc2e9a157b3a9351bab60
jigglypop <ydh2244@gmail.com>
2020-07-26 22:37:44 +0900

    0726

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Layout from "~/components/layout";
 import Home from "~/components/Home";
+import Helmet from "react-helmet";
 
 const HomeLayout = (props) => (
   <Layout {...props}>
+    <Helmet>
+      <meta
+        name="google-site-verification"
+        content="zaOldSWebTfa4WLB9Z4icGl9cMyKpKPf38uTTEv2whg"
+      />
+    </Helmet>
     <Home />
   </Layout>
 );
