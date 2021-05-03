924aaacdb4ea003d24ad0c6d184883f4f6a996fe
jigglypop <ydh2244@gmail.com>
2021-05-03 20:41:49 +0900

    0503

--- src/api/server_url.ts
+++ src/api/server_url.ts
@@ -1,3 +1,4 @@
-export const SERVER_URL = 'http://ec2-13-124-60-125.ap-northeast-2.compute.amazonaws.com:3000'
+// export const SERVER_URL = 'https://ec2-13-124-60-125.ap-northeast-2.compute.amazonaws.com:3000'
 
 // export const SERVER_URL = 'http://localhost:3000'
+export const SERVER_URL = 'http://www.moija.link:3000'
