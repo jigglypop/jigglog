3be045e69a45cc928dbba057dea11c2cadffacbc
jigglypop <ydh2244@gmail.com>
2021-06-17 18:26:07 +0900

    0617

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./styled.css";
 import { PostContent } from "../Post/styled";
+import * as fs from 'fs';
 
 const PREFIX = "jigglog| ";
 
